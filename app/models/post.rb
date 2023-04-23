c42284963ca31f80aba116076d0dbe2e6bd83157
stxss <93729036+stxss@users.noreply.github.com>
2023-04-23 19:33:53 +0100

    Create post model

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 500 }
+
+  belongs_to :user
+  has_many :comments
+end
